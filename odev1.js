5881426ac20a3c22257dde53875285b4f82c086a
goksu1 <goksu.baykal@gmail.com>
2022-09-03 00:44:34 +0300

    Odev1

--- odev1.js
+++ odev1.js
@@ -9,7 +9,20 @@
 
 //Test :
 
-const array = ["Patika","219","Akbank","React","Bootcamp"]
+const array = ["Patika", "219", "Akbank", "React", "Bootcamp"]
+Array.prototype.includesCi = function (search) {
+    if (this.find((element) => element.toLowerCase() === search.toLowerCase())) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+console.log(array.includesCi("patika") === true ? "Beklendiği gibi" : "Beklendiği gibi değil")
+console.log(array.includesCi("219") === false ? "Beklendiği gibi" : "Beklendiği gibi değil")
+console.log(array.includesCi("kırmızı") === false ? "Beklendiği gibi" : "Beklendiği gibi değil")
 
-console.log(array.includesCi("patika")===true ? "Beklendiği gibi" : "Beklendiği gibi değil")
-console.log(array.includesCi("kırmızı")===false ? "Beklendiği gibi" : "Beklendiği gibi değil")
+// includes(), bir dizinin girdileri arasında belirli bir değer içerip içermediğini, dönen true veya false ile uygun şekilde belirler.
+// toLowerCase ile aranan ve bulunan elementleri küçük harflere çevirdik. (Case sensitivity) 
+// Böylelikle harfin küçüklüğüne ve büyüklüğüne bakmadan aranan element varsa doğru yoksa yanlış olarak döndürdük. 
+// Kaynak : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
